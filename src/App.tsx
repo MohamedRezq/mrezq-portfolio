6b3a6fa8e56575054d2b2fcd4b1a2ad92a940ecc
MohamedRezq <mrezq.dev@gmail.com>
2023-09-05 02:44:13 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+//--> BEGIN OF IMPORTS <--------------------------------------------------//
+
+//--> END OF IMPORTS <----------------------------------------------------//
+
+function App() {
+  return <div>App</div>;
+}
+
+export default App;
